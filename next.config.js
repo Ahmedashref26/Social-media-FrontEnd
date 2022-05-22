ee311c157c1af6e2850af72a86c3c5836ecaf3d8
Ahemd-Ashref <ahmedashref26@gmail.com>
2022-05-22 11:13:59 +0200

    modify next.config

--- next.config.js
+++ next.config.js
@@ -3,32 +3,32 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 const path = require('path');
 
 module.exports = (phase, { defaultConfig }) => {
-  // if (phase === PHASE_DEVELOPMENT_SERVER) {
-  //   return {
-  //     reactStrictMode: true,
-  //     env: {
-  //       JWT_SECRET: 'the-awosome-and-most-secure-web-token-secret',
-  //       NEXTAUTH_URL: 'http://localhost:3000/',
-  //       API_URL: 'http://localhost:8800',
-  //       PUBLIC_FOLDER: 'http://localhost:8800/images',
-  //     },
-  //     sassOptions: {
-  //       includePaths: [path.join(__dirname, 'styles')],
-  //       prependData: `@import "_variables.scss";`,
-  //     },
-  //     images: {
-  //       domains: ['localhost'],
-  //     },
-  //     async rewrites() {
-  //       return [
-  //         {
-  //           source: '/api/v1/:slug*',
-  //           destination: `http://localhost:8800/api/v1/:slug*`,
-  //         },
-  //       ];
-  //     },
-  //   };
-  // }
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      env: {
+        JWT_SECRET: 'the-awosome-and-most-secure-web-token-secret',
+        NEXTAUTH_URL: 'http://localhost:3000/',
+        API_URL: 'http://localhost:8800',
+        PUBLIC_FOLDER: 'http://localhost:8800/images',
+      },
+      sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+        prependData: `@import "_variables.scss";`,
+      },
+      images: {
+        domains: ['localhost'],
+      },
+      async rewrites() {
+        return [
+          {
+            source: '/api/v1/:slug*',
+            destination: `http://localhost:8800/api/v1/:slug*`,
+          },
+        ];
+      },
+    };
+  }
 
   return {
     reactStrictMode: true,
