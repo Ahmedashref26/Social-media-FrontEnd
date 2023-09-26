8111de780fcb15e7f03479f00385674d0e2eead0
Ahmed Ashref <73080058+Ahmedashref26@users.noreply.github.com>
2023-09-26 08:31:00 +0300

    [change] api url

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-const path = require('path');
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+const path = require("path");
 
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       reactStrictMode: true,
       sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
+        includePaths: [path.join(__dirname, "styles")],
         prependData: `@import "_variables.scss";`,
       },
       images: {
-        domains: ['localhost'],
+        domains: ["localhost"],
       },
       async rewrites() {
         return [
           {
-            source: '/api/v1/:slug*',
+            source: "/api/v1/:slug*",
             destination: `http://localhost:8800/api/v1/:slug*`,
           },
         ];
@@ -27,17 +27,17 @@ module.exports = (phase, { defaultConfig }) => {
   return {
     reactStrictMode: true,
     sassOptions: {
-      includePaths: [path.join(__dirname, 'styles')],
+      includePaths: [path.join(__dirname, "styles")],
       prependData: `@import "_variables.scss";`,
     },
     images: {
-      domains: ['api-gosocial.herokuapp.com'],
+      domains: ["social-media-api.up.railway.app"],
     },
     async rewrites() {
       return [
         {
-          source: '/api/v1/:slug*',
-          destination: `https://api-gosocial.herokuapp.com/api/v1/:slug*`,
+          source: "/api/v1/:slug*",
+          destination: `https://social-media-api.up.railway.app/api/v1/:slug*`,
         },
       ];
     },
